9d06a3cd4aa266896e7a7b6a8190216fd96dfcca
Mikkel O. Laursen <modl@seges.dk>
2022-12-06 14:43:50 +0100

    Add first solution

---
+++ main.ts
@@ -0,0 +1,12 @@
+import * as fs from "node:fs";
+
+fs.readdir("./calendar", (_err, files) => {
+  files.forEach((file) => {
+    const solution = require(`./calendar/${file}`);
+
+    console.log(
+      `Solution for day ${file.slice(0, file.indexOf("."))}`,
+      solution
+    );
+  });
+});
